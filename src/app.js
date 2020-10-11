7e2d003d9a0cbaec9de4826b4c0736ab848e60a7
Tess Gronski <tessgronski@gmail.com>
2020-10-11 14:15:01 -0700

    Adds validation

--- src/app.js
+++ src/app.js
@@ -7,31 +7,38 @@ const app = express()
 
 app.use(cors())
 
-app.get('/', (request, response) => {
+app.get('/', (request, response,next) => {
     let apiKey='c4861f90-7f85-4f56-9330-183e75299607';
     let hubspot = new Hubspot({ apiKey: apiKey });
     hubspot.contacts.getAll().then(results => {
         response.send(results)
-      })
+      }).catch(next);
 
 })
 
-app.post("/",  jsonParser,(req,res)=>{
+app.post("/",  jsonParser,(req,res,next)=>{
     let apiKey='c4861f90-7f85-4f56-9330-183e75299607';
     let hubspot = new Hubspot({ apiKey: apiKey });
     let {firstname, email, phone}=req.body;
+    if(phone.length<10){
+        console.log('phone error')
+        return res.status(400).json({error: "Phone number error. Please try again"})
+    }
+
+
     let newContact = {"properties":[
             { "property": "firstname","value": firstname },
             { "property": "email", "value": email},
             { "property": "phone", "value": phone }
     ]
     }
-    
-    hubspot.contacts.create(newContact).then(results=>{
-        res.send(results)
-    });
 
-
+    hubspot.contacts.create(newContact)
+    .then(newContact =>{
+        return res.json({
+            error: 'Information added to our database!',
+          });
+    }).catch(next)
 })
 
 
