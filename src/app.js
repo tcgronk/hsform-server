8dbfc1aed230bad1e3f6260edc91528af5553d4c
Tess Gronski <tessgronski@gmail.com>
2020-10-11 17:46:46 -0700

    Moves api key to .env file

--- src/app.js
+++ src/app.js
@@ -5,40 +5,28 @@ const cors = require('cors')
 const helmet = require("helmet");
 const jsonParser = express.json();
 const app = express()
+const { api_key } = require("./config");
 
 app.use(cors())
 app.use(helmet());
 app.use(express.json());
 
 
-app.get('/', (request, response,next) => {
-    let apiKey='c4861f90-7f85-4f56-9330-183e75299607';
-    let hubspot = new Hubspot({ apiKey: apiKey });
-    hubspot.contacts.getAll().then(results => {
-        response.send(results)
-      }).catch(next);
-
-})
 
 app.post("/",  jsonParser,(req,res,next)=>{
-    let apiKey='c4861f90-7f85-4f56-9330-183e75299607';
-    let hubspot = new Hubspot({ apiKey: apiKey });
-    let {firstname, email, phone}=req.body;
-    if(phone.length<10){
-        console.log('phone error')
-        return res.status(400).json({error: "Phone number error. Please try again"})
-    }
-
-
+    let {firstname,lastname,company, email, phone}=req.body;
     let newContact = {"properties":[
             { "property": "firstname","value": firstname },
+            { "property": "lastname","value": lastname },
             { "property": "email", "value": email},
-            { "property": "phone", "value": phone }
+            { "property": "phone", "value": phone },
+            { "property": "company", "value": company }
     ]
     }
-
+    const hubspot = new Hubspot({ apiKey: api_key });
     hubspot.contacts.create(newContact)
     .then(newContact =>{
+        console.log(newContact)
         return res.json({
             error: 'Information added to our database!',
           });
